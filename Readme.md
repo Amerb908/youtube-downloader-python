98c7aa8c38528789e4ff521d72191ebe27d189f0
Amerb908 <ab1571514@gmail.com>
2024-12-11 06:49:14 -0800

    Uploading project

---
+++ Readme.md
@@ -0,0 +1,21 @@
+## Youtube MP4 Downloader
+
+### This is a simple application where users can download YouTube videos, and the videos will be saved in the 'Downloaded videos' directory by default. You can modify the directory path to save videos anywhere you'd like.
+
+---
+
+### Features
+- User-friendly interface built with **Tkinter**.
+- Downloads YouTube videos directly to your system.
+- Customizable download location via the `ydl_opts` configuration.
+
+---
+
+### Requirements
+Before running the application, ensure you have the following installed:
+
+- **Python 3.x**
+- **yt-dlp**: A Python library for downloading videos. You can install it via pip:
+
+  ```bash
+  pip install yt-dlp
---
+++ app.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from tkinter import messagebox
+import yt_dlp
+
+def download_video():
+    url = url_entry.get()
+    if url:
+        try:
+            ydl_opts = {
+                'outtmpl': 'downloaded vid\\%(title)s.%(ext)s'  #choose the location you want to save your video. By default, it will save it in the current directory
+            }
+            #this downloads the video
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+            messagebox.showinfo("Success", "Video downloaded successfully!")
+        except Exception as e:
+            messagebox.showerror("Error", str(e))
+    else:
+        messagebox.showerror("Error", "Please enter a valid URL")
+
+root = tk.Tk()
+root.title("YouTube Video Downloader")
+root.geometry("400x200")
+
+url_label = tk.Label(root, text="Enter YouTube Video URL:")
+url_label.pack()
+
+url_entry = tk.Entry(root, width=50)
+url_entry.pack()
+
+download_button = tk.Button(root, text="Download as MP4", command=download_video)
+download_button.pack()
+
+root.mainloop()
+
